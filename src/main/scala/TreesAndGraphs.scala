a43d171065ece094a1761399e8e12356bad62db3
nivnatan <nivnatan@gmail.com>
2017-12-12 09:37:38 +0200

    .

--- src/main/scala/TreesAndGraphs.scala
+++ src/main/scala/TreesAndGraphs.scala
@@ -5,20 +5,20 @@ object TreesAndGraphs extends App {
 
   case class TreeNode[T](data: T, var left: Option[TreeNode[T]] = None, var right: Option[TreeNode[T]] = None)
 
-  val node1 = TreeNodeLinkedList(10)
-  val node2 = TreeNodeLinkedList(5)
-  val node3 = TreeNodeLinkedList(20)
-  val node4 = TreeNodeLinkedList(25)
-  val node5 = TreeNodeLinkedList(15)
-  val node6 = TreeNodeLinkedList(2)
-  val node7 = TreeNodeLinkedList(6)
+  val node1 = TreeNode(10)
+  val node2 = TreeNode(5)
+  val node3 = TreeNode(20)
+  val node4 = TreeNode(25)
+  val node5 = TreeNode(15)
+  val node6 = TreeNode(2)
+  val node7 = TreeNode(6)
 
   node1.left  = Some(node2) // B
   node1.right = Some(node3) // C
-//  node2.left  = Some(node6) // D
-//  node2.right = Some(node7) // E
-//  node3.left  = Some(node5) // F
-//  node3.right = Some(node4) // G
+  node2.left  = Some(node6) // D
+  node2.right = Some(node7) // E
+  node3.left  = Some(node5) // F
+  node3.right = Some(node4) // G
 
   /**
     *              A
@@ -77,47 +77,54 @@ object TreesAndGraphs extends App {
     1 + Math.max(root.left.map(treeHeightMoreElegant).getOrElse(0), root.right.map(treeHeightMoreElegant).getOrElse(0))
   }
 
-  case class TreeNodeRouteSum(data: Int, var left: Option[TreeNodeRouteSum] = None, var right: Option[TreeNodeRouteSum] = None, var sumRoute: Int = 0)
   /**
-    * Find tree height (number of nodes from the root until the farthest leaf)
-    * e.g. Input : 23, 34,56,21,21,56,78,23, 34
-    * Output: 23
+    * For each node, set the sumRoute to be the sum of all nodes visited during the route to that node
+    */
+  object TreeNodesRouteSum {
+    case class TreeNodeRouteSum(data: Int, var left: Option[TreeNodeRouteSum] = None, var right: Option[TreeNodeRouteSum] = None, var sumRoute: Int = 0)
+
+    def routeSum(root: TreeNodeRouteSum) = {
+      def routeSumRec(root: TreeNodeRouteSum, routeSum: Int): Unit = {
+        root.sumRoute = root.data + routeSum
+        root.left.map(routeSumRec(_, root.sumRoute))
+        root.right.map(routeSumRec(_, root.sumRoute))
+      }
+      routeSumRec(root, 0)
+    }
+  }
+
+  /**
+    * Create a linked list out of the tree nodes as if they were to appear during in-inorder traversal
+    * The list should not be returned, but rather be updated using the *next variable
+    * The idea - first we need to find the head of the list which is the leftmost leaf.
+    * then we iterate using the inorder traversal template, and each time, we reach the tail of the list and append the new node
+    * For example -
+    *              A
+    *           /    \
+    *         /       \
+    *        B         C
     *
-    * @param root
-    * @return number which is not repeated in the array of integers
+    * B(next) -> A(next) -> C
     */
-  def routeSum(root: TreeNodeRouteSum) = {
-    def routeSumRec(root: TreeNodeRouteSum, routeSum: Int): Unit = {
-      root.sumRoute = root.data + routeSum
-      root.left.map(routeSumRec(_, root.sumRoute))
-      root.right.map(routeSumRec(_, root.sumRoute))
+  object TreeNodeLinkedList {
+    case class TreeNodeLinkedList[T](data: T, var left: Option[TreeNodeLinkedList[T]] = None, var right: Option[TreeNodeLinkedList[T]] = None, var next: Option[TreeNodeLinkedList[T]] = None) {
+      override def toString = s"currentNode=${data.toString}, listNext=${next}"
     }
-    routeSumRec(root, 0)
-  }
 
-  case class TreeNodeLinkedList[T](data: T, var left: Option[TreeNodeLinkedList[T]] = None, var right: Option[TreeNodeLinkedList[T]] = None, var next: Option[TreeNodeLinkedList[T]] = None) {
-    override def toString = ""
-  }
-  /**
-    * Find tree height (number of nodes from the root until the farthest leaf)
-    * e.g. Input : 23, 34,56,21,21,56,78,23, 34
-    * Output: 23
-    * https://cboard.cprogramming.com/c-programming/108712-changing-tree-into-list-question.html
-    * https://www.cs.bgu.ac.il/~prog141/wiki.files/prog141_ps11.pdf
-    * @param root
-    * @return number which is not repeated in the array of integers
-    */
-  def tree2List[Int](root: TreeNodeLinkedList[Int]): Unit = {
-    def tree2ListRec(root: TreeNodeLinkedList[Int]): Option[TreeNodeLinkedList[Int]] = {
-      root.next = root.right.map(tree2ListRec(_)).getOrElse(None)
-
-      root.left.map(tree2ListRec(_, Some(root)))
-      root.next = prev
-      root.right.map(tree2ListRec(_, Some(root)))
+    def tree2List[_](root: TreeNodeLinkedList[_]): TreeNodeLinkedList[_] = {
+      def tree2ListRec(root: TreeNodeLinkedList[_], headOfList: TreeNodeLinkedList[_]): Unit = {
+        root.left.map(tree2ListRec(_,headOfList))
+        if(root != headOfList) {
+          var tailList = headOfList
+          while(tailList.next.isDefined) tailList = tailList.next.get
+          tailList.next = Some(root)
+        }
+        root.right.map(tree2ListRec(_,headOfList))
+      }
+      def initHeadOfListWhichIsTheLeftMostLeaf(root: TreeNodeLinkedList[_]): TreeNodeLinkedList[_] = root.left.map(initHeadOfListWhichIsTheLeftMostLeaf).getOrElse(root)
+      val headOfList = initHeadOfListWhichIsTheLeftMostLeaf(root)
+      tree2ListRec(root, headOfList)
+      headOfList
     }
-    tree2ListRec(root, None)
   }
-
-  val asdas = tree2List(node1)
-  val adsa = "erh"
 }
