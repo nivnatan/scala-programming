cc3ba947a227523fc098f7fe2d68aca6b8f17dd3
nivmmg <niv@m-m-g.com>
2018-01-15 11:13:21 +0200

    .

--- src/main/scala/exercises/Algorithms.scala
+++ src/main/scala/exercises/Algorithms.scala
@@ -1,5 +1,9 @@
 package exercises
 
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
+
 /**
   * Created by niv on 12/01/2018.
   */
@@ -45,6 +49,7 @@ object Algorithms extends App {
     * 2,5,1,6 (i=3,j=2)
     * 2,1,5,6 (i=3,j=1)
     * 1,2,5,6 (i=3,j=0)
+    *
     * @param arr
     */
   def insertionSort(arr: Array[Int]) = {
@@ -67,6 +72,7 @@ object Algorithms extends App {
     * All elements greater than the pivot must be in the second partition.
     * Use recursion to sort both partitions.
     * Join the first sorted partition, the pivot, and the second sorted partition.
+    *
 
     * @param arr
     */
@@ -126,5 +132,56 @@ object Algorithms extends App {
     }
 
     runQuickSort(0, arr.size - 1)
+  }
+
+  /**
+    * Design an advanced data structure which has all the functionalities of a set and
+    * additionally it is also able to return a random number from the set in constant time.
+    * the data structure should have the following operations in O(1) time:
+    * insert, remove, getRandom, contains
+    * Solution -
+    * **********
+    * use both HashMap and ArrayList / Array / Double LinkedList.
+    * insert :- Check if X is already present in HashMap --Time complexity O(1) . if not Present Then Add in end of ArrayList -- Time complexity O(1). add it in HashMap also x as key and last Index as a value -- Time complexity O(1).
+    * remove :- Check if X is present in HashMap --Time complexity O(1). If present then find the its index and remove it from HashMap --Time complexity O(1). swap this element with last element in ArrayList and remove the last element --Time complexity O(1). Update the index of last Element in HashMap --Time complexity O(1).
+    * getRandom :- Generate Random number from 0 to last index of ArrayList . return the ArrayList element at random index generated --Time complexity O(1).
+    * contains :- See in HashMap for x as a key. --Time complexity O(1).
+    */
+  class SetWithConstantRandomElementFunctionality {
+    private val array = new ArrayBuffer[Any]
+    private val map   = mutable.Map.empty[Any,Int]
+
+    def insert(value: Any) = {
+      map.get(value) match {
+        case None => {
+          val index = array.size
+          array += value
+          map.put(value,index)
+        }
+        case _ =>
+      }
+    }
+
+    def delete(value: Any) = {
+      map.get(value) match {
+        case Some(index) => {
+          val lastValueIndex = array.size - 1
+          if(index == lastValueIndex) {
+            array.remove(index)
+          } else {
+            val lastValue = array(lastValueIndex)
+            array(index) = lastValue
+            array.remove(lastValueIndex)
+            map.put(lastValue, index)
+          }
+          map.remove(value)
+        }
+        case _ =>
+      }
+    }
+
+    def getRandom = if(array.isEmpty) throw new RuntimeException("empty array") else array(new Random().nextInt(array.size))
+
+    def contains(value: Any) = map.contains(value)
   }
 }
