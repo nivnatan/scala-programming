4b159e56077dd8e504421d4d94a9d9d5a154c479
nivmmg <niv@m-m-g.com>
2018-02-06 10:05:43 +0200

    .

--- src/main/scala/exercises/ArraysAndStrings.scala
+++ src/main/scala/exercises/ArraysAndStrings.scala
@@ -1451,4 +1451,18 @@ object ArraysAndStrings extends App {
 
     keepTrackOfMinimumElementSolution
   }
+
+  /**
+    * Given an infinite supply of ‘m’ coin denominations S[m] = {S1, S2... Sm}, calculate all the different combinations which can be used to get change for some quantity ‘N’
+    * So For:
+    * if N = 4 and S = {1,2,3}, then different ways possible are
+    * {1,1,1,1}, {2,1,1}, {3,1}, {2,2}
+    *
+    * @param coins
+    * @param coinsCount
+    * @param sum
+    */
+  def allCombinationsToSumCoins(coins: Array[Int], coinsCount: Int, sum: Int): Int = {
+    -1
+  }
 }
