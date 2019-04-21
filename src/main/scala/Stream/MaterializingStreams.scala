1b9f9ecf9c5b95ce3e6f3d4fc0aa841745002282
Niv <nivnatan@gmail.com>
2019-04-21 21:08:44 +0300

    .

--- src/main/scala/Stream/MaterializingStreams.scala
+++ src/main/scala/Stream/MaterializingStreams.scala
@@ -11,6 +11,7 @@ object MaterializingStreams extends App {
 
   implicit val actorSystem = ActorSystem()
   implicit val materializer = ActorMaterializer()
+  import actorSystem.dispatcher
 
   // choosing materialized values
   val simpleSource = Source(1 to 10)
@@ -36,4 +37,29 @@ object MaterializingStreams extends App {
     * - compute the total word count out of a stream of sentences
     * map, fold, reduce
     */
+
+  val f1 = Source(1 to 10).toMat(Sink.last)(Keep.right).run()
+  val f2 = Source(1 to 10).runWith(Sink.last)
+
+  val setenceSource = Source(List(
+    "Akka is awesome",
+    "I love streams",
+    "Materilized values are kiliiiiiing who 2222"
+  ))
+
+  val wordCountSink = Sink.fold[Int, String](0)((currentWords, newSentence) => currentWords + newSentence.split( "").length)
+  val g1 = setenceSource.toMat(wordCountSink)(Keep.right).run()
+  val g2 = setenceSource.runWith(wordCountSink)
+  val g3 = setenceSource.runFold(0)((currentWords, newSentence) => currentWords + newSentence.split(" ").length)
+
+  val wordCountFlow = Flow[String].fold[Int](0)((currentWords, newSentences) => currentWords + newSentences.split(" ").length)
+  val g4 = setenceSource.via(wordCountFlow).toMat(Sink.head)(Keep.right).run()
+  val g5 = setenceSource.viaMat(wordCountFlow)(Keep.right).toMat(Sink.head)(Keep.right).run()
+  val g6 = setenceSource.via(wordCountFlow).runWith(Sink.head)
+  val g7 = wordCountFlow.runWith(setenceSource, Sink.head)._2
+
+  g4.onComplete {
+    result => println(result)
+  }
+
 }
