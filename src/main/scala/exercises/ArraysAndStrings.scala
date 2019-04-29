87b3bf429e9c7f5d999adb140738e6a15c6da8e5
nivnatan <33713161+nivnatan@users.noreply.github.com>
2019-04-29 17:16:55 +0300

    Update ArraysAndStrings.scala

--- src/main/scala/exercises/ArraysAndStrings.scala
+++ src/main/scala/exercises/ArraysAndStrings.scala
@@ -2348,4 +2348,24 @@ object ArraysAndStrings extends App {
 
     arr
   }
+ 
+   /**
+    * Return a new sorted merged list from K sorted lists, each with size N
+    * Example : [[10, 15, 30], [12, 15, 20], [17, 20, 32]]
+    * Result : [10, 12, 15, 15, 17, 20, 20, 30, 32]
+    *
+    * @param list
+    * @param target
+    */
+  def newSortedMergedListFromKSortedListsEachWithSizeN(list: List[List[Int]]): List[Int] = {
+    list.flatten.sorted
+//    Another efficient solution is to use Min Heap. This Min Heap based solution has same time complexity which is O(nk Log k). But for different sized arrays, this solution works much better.
+//
+//      Following is detailed algorithm.
+//    1. Create an output array of size n*k.
+//    2. Create a min heap of size k and insert 1st element in all the arrays into the heap
+//      3. Repeat following steps n*k times.
+//    a) Get minimum element from heap (minimum is always at root) and store it in output array.
+//      b) Replace heap root with next element from the array from which the element is extracted. If the array doesn’t have any more elements, then replace root with infinite. After replacing the root, heapify the tree.
+  }
 }
