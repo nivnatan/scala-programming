f63fd2a295ffb53848dbdb39219d269478eb9962
nivnatan <33713161+nivnatan@users.noreply.github.com>
2021-02-15 22:22:13 +0200

    Update ArraysAndStrings.scala

--- src/main/scala/exercises/ArraysAndStrings.scala
+++ src/main/scala/exercises/ArraysAndStrings.scala
@@ -3061,4 +3061,27 @@ object ArraysAndStrings extends App {
 
     run(0, 1)
   }
+ 
+  /**
+    * Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+    * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+    * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+    *
+    * Input: nums = [3,2,2,3], val = 3
+    * Output: 2, nums = [2,2]
+    * Explanation: Your function should return length = 2, with the first two elements of nums being 2.
+    * It doesn't matter what you leave beyond the returned length. For example if you return 2 with nums = [2,2,3,3] or nums = [2,2,0,0], your answer will be accepted.
+    */
+  def removeElement(nums: Array[Int], `val`: Int): Int = {
+    var left = 0
+    var right = nums.length - 1
+    while(left <= right) {
+      if(nums(left) == `val`) {
+        nums(left) = nums(right)
+        right -= 1
+      } else left += 1
+    }
+    right + 1
+  }
+}
 }
