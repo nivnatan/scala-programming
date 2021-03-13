74da20e48d657982ffee250b1a3a07647ef5b9a3
nivnatan <33713161+nivnatan@users.noreply.github.com>
2021-03-13 20:27:05 +0200

    Update ArraysAndStrings.scala

--- src/main/scala/exercises/ArraysAndStrings.scala
+++ src/main/scala/exercises/ArraysAndStrings.scala
@@ -3223,4 +3223,87 @@ object ArraysAndStrings extends App {
 
     run(digits.length - 1)
   }
+
+  /**
+    * Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+    * The number of elements initialized in nums1 and nums2 are m and n respectively.
+    * You may assume that nums1 has a size equal to m + n such that it has enough space to hold additional elements from nums2.
+    *
+    * Input: nums1 = [1,2,3,4,0,0,0], m = 4, nums2 = [2,5,6], n = 3
+    * Output: [1,2,2,3,4,5,6]
+    *
+    * Input: nums1 = [4,5,6,0,0,0], m = 4, nums2 = [1,2,3], n = 3
+    * Output: [1,2,3,4,5,6]
+    */
+  def merge(nums1: Array[Int], m: Int, nums2: Array[Int], n: Int): Unit = {
+
+    object usingExtraMemory {
+
+      def run(): Unit = {
+        var x,y = 0
+        val res = ArrayBuffer.empty[Int]
+
+        while(x < m && y < n) {
+          if(nums1(x) <= nums2(y)) {
+            res += nums1(x)
+            x += 1
+          } else {
+            res += nums2(y)
+            y += 1
+          }
+        }
+
+        while(x < m) {
+          res += nums1(x)
+          x += 1
+        }
+
+        while(y < n) {
+          res += nums2(y)
+          y += 1
+        }
+
+        var w = 0
+        while(w < m + n){
+          nums1(w) = res(w)
+          w += 1
+        }
+      }
+    }
+    
+    // idea - run both indexes from the end (reverse order)
+    object inPlace {
+
+      def run(): Unit = {
+        var x = m - 1
+        var y = n - 1
+        var w = m + n - 1
+
+        while (x >= 0 && y >= 0) {
+          if (nums1(x) < nums2(y)) {
+            nums1(w) = nums2(y)
+            y -= 1
+          } else {
+            nums1(w) = nums1(x)
+            x -= 1
+          }
+          w -= 1
+        }
+
+        while(x >= 0) {
+          nums1(w) = nums1(x)
+          x -= 1
+          w -= 1
+        }
+
+        while(y >= 0) {
+          nums1(w) = nums2(y)
+          y -= 1
+          w -= 1
+        }
+      }
+    }
+
+    inPlace.run()
+  }
 }
