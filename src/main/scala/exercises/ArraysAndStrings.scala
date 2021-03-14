023d50040fdfe8654e64ed0523b3273293ebfe6e
nivnatan <33713161+nivnatan@users.noreply.github.com>
2021-03-14 06:23:03 +0200

    Update ArraysAndStrings.scala

--- src/main/scala/exercises/ArraysAndStrings.scala
+++ src/main/scala/exercises/ArraysAndStrings.scala
@@ -3313,4 +3313,44 @@ object ArraysAndStrings extends App {
 
     inPlace.run()
   }
+
+  /**
+    * Given an integer numRows, return the first numRows of Pascal's triangle.
+    * In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+    *
+    * Input: Input: numRows = 5
+    * Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+    *
+    * Input: numRows = 1
+    * Output: [[1]]
+    */
+
+  def generate(numRows: Int): List[List[Int]] = {
+
+    def generate(prev: List[Int]): List[Int] = {
+
+      def generate(prev: List[Int], res: List[Int]): List[Int] = {
+        prev match {
+          case Nil    => res
+          case h :: t => generate(t, res ::: List(h + t.headOption.getOrElse(0)))
+        }
+      }
+
+      generate(prev, List(1))
+    }
+
+    def run(num: Int, prev: List[Int], res: List[List[Int]]): List[List[Int]] = {
+
+      if(num > numRows) res
+      else {
+        val prevUpdated = generate(prev)
+        run(num + 1, prevUpdated, res ::: List(prevUpdated))
+      }
+    }
+
+    numRows match {
+      case 1 => List(List(1))
+      case _ => run(2, List(1), List(List(1)))
+    }
+  }
 }
